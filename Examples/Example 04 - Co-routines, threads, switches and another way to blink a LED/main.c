6ae93336b4b2fd0111cc7b56bf5887c5b12e6c26
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2020-02-27 18:12:50 -0300

    Update main.c

--- Examples/Example 04 - Co-routines, threads, switches and another way to blink a LED/main.c
+++ Examples/Example 04 - Co-routines, threads, switches and another way to blink a LED/main.c
@@ -1,10 +1,9 @@
 //! Example 04 - Co-routines, threads, switches and another way to blink a LED
 /*!
   This simply example demonstrates how to share a variable between tasks.
-
   This code file was written by Jorge Henrique Moreira Santana and is under
   the GNU GPLv3 license. All legal rights are reserved.
-
+  
   Permissions of this copyleft license are conditioned on making available
   complete source code of licensed works and modifications under the same
   license or the GNU GPLv3. Copyright and license notices must be preserved.
@@ -13,24 +12,24 @@
   work using the licensed work through interfaces provided by the licensed
   work may be distributed under different terms and without source code for
   the larger work.
-
+  
   * Permissions:
     -> Commercial use;
     -> Modification;
     -> Distribuition;
     -> Patent Use;
     -> Private Use;
-
+    
   * Limitations:
     -> Liability;
     -> Warranty;
-
+    
   * Conditions:
     -> License and copyright notice;
     -> Disclose source;
     -> State changes;
     -> Same license (library);
-
+    
   For more informations, check the LICENSE document. If you want to use a
   commercial product without having to provide the source code, send an email
   to jorge_henrique_123@hotmail.com to talk.
@@ -184,7 +183,7 @@ int main(){
     tTaskFlag switch declaration.
   */
   switch_t sTaskFlag;
-  newSwitch(&sTaskFlag);
+  newSwitch(sTaskFlag);
 
   /*!
     Tasks installation.
