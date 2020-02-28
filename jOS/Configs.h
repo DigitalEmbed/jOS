e9d64a0a20f2715167019705d3e1a9e2ed58ee94
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2020-02-28 14:50:44 -0300

    Update Configs.h

--- jOS/Configs.h
+++ jOS/Configs.h
@@ -6,6 +6,7 @@
   Permissions of this copyleft license are conditioned on making available
   complete source code of licensed works and modifications under the same
   license or the GNU GPLv3. Copyright and license notices must be preserved.
+  
   Contributors provide an express grant of patent rights. However, a larger
   work using the licensed work through interfaces provided by the licensed
   work may be distributed under different terms and without source code for
@@ -48,8 +49,9 @@
     - ArduinoAVR
     - HALru
     - ESP12
+    - SystickSTM32HAL
   */
-  #define ArduinoAVR
+  #define NoMicrocontroller
 
   //! Configuration: RTOS Auto Initialization
   /*!
@@ -57,7 +59,7 @@
       - __AUTO_INITIALIZATION_ENABLE__
       - __AUTO_INITIALIZATION_DISABLE__
   */
-  #define __AUTO_INITIALIZATION_ENABLE__
+  #define __AUTO_INITIALIZATION_DISABLE__
 
   //! Configuration: Task Manager
   /*!
