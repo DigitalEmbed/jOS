f3fb238a0bec0f5cfaa79711fa4b02be1f4214ef
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2020-03-01 11:21:29 -0300

    Update Task.c

--- jOS+/Task.c
+++ jOS+/Task.c
@@ -1,17 +1,13 @@
 #include "./Task.h"
 
 /*!
-  Task object "constructor".
-*/
-
-/*!
   Prototype private "methods".
 */
 static void __delayMS(uint16_t ui16TimeMS);
 static void __waitFunction(bool bCondition);
 static void __yield(void);
 
-#if defined(__AVR)
+#if defined(__AVR__)
   const task_manager_t Task PROGMEM = {
 #else
   const task_manager_t Task = {
